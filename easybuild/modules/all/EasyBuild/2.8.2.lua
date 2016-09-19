c4f75cdeab014af55e55b06477df66af1353059a
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2016-09-19 14:08:57 +0300

    Added configuration for easybuild

--- easybuild/modules/all/EasyBuild/2.8.2.lua
+++ easybuild/modules/all/EasyBuild/2.8.2.lua
@@ -22,16 +22,17 @@ conflict("EasyBuild")
 
 -- TODO: version numbers need to be determined automagically.
 local version = "2.8.2"
-local base = "~/CentOS7/easybuild/software/EasyBuild/" .. version
+local home = os.getenv("HOME")
+local base =  home .. "/CentOS7/easybuild/software/EasyBuild/" .. version
 local binpath = base .. "/bin"
 local libpath = base .. "/lib"
 local develpath = base .. "/easybuild/EasyBuild-" .. version .. "-easybuild-devel"
 local pythonpath = libpath .. "/python2.7/site-packages"
-local configpath = "~/CentOS7/config"
+local configpath = home .. "/CentOS7/config/easybuild.cfg"
 
 prepend_path("PATH", binpath)
+prepend_path("PYTHONPATH", pythonpath)
 setenv("EBROOTEASYBUILD", base)
 setenv("EBVERSIONEASYBUILD", version)
 setenv("EBDEVELEASYBUILD", develpath)
 setenv("EASYBUILD_CONFIGFILES", configpath)
-prepend_path("PYTHONPATH", pythonpath)
