b5e7ad52d7b5c31023d9ab11001e0a3e0c262da3
Madhu Srinivasan <madhu.srinivasan@vyslexia.com>
2018-03-26 15:38:33 +0300

    Mapd 3.4 added

---
+++ modulefiles/mapd/3.4.0.lua
@@ -0,0 +1,5 @@
+load("cuda/9.0")
+setenv("MAPD_USER",os.getenv("USER"))
+setenv("MAPD_GROUP","g-srinivm")
+setenv("MAPD_STORAGE","/home/srinivm/.mapd/mapd")
+setenv("MAPD_PATH","/home/srinivm/CentOS7/software/mapd/3.4.0-gpu")
