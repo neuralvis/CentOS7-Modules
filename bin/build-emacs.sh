3000764823f7e6e47935c84e6419fe758f894787
Madhu Srinivasan <msrinivasa@cray.com>
2020-07-17 14:06:09 -0500

    updates

---
+++ bin/build-emacs.sh
@@ -0,0 +1,3 @@
+./configure --prefix=/cls03003/msrinivasa/develop/CentOS7-Modules/software/emacs/26.3 --with-x-toolkit=no --with-xpm=no --with-gif=no --with-tiff=no  --without-jpeg --without-x --with-gnutls=yes
+
+make -j8
